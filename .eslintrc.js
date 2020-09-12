665d8a2adb18aa6c0b4369666ed5b45f5217e912
Jakub Mazurek (Jayu) <jakub.mazurek.dev@gmail.com>
2020-09-12 12:05:50 +0200

    chore: initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 0
+  }
+};
